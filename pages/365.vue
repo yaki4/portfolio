5570041cc3f49ce04246bab67a78b90c16e95802
gabriel <gabriel.corbel@gmail.com>
2020-01-06 22:46:20 -0500

    first

---
+++ pages/365.vue
@@ -0,0 +1,15 @@
+<template lang="pug">
+  div
+    section(v-for='(color, index) in colors', :key='index')
+</template>
+
+<script>
+
+export default {
+  asyncData() {
+    return {
+      colors: ['#D53738', '#638867', '#FAF429']
+    }
+  }
+}
+</script>
